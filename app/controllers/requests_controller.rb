0261544d2f0a121cefd23dbe3e66105dff41493f
bickelmann brice <bickelmann.b@gmail.com>
2019-07-03 17:57:24 +0200

    controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,10 +1,28 @@
 class RequestsController < ApplicationController
   def index
+    @requests = Request.all
+  end
+
+  def show
+    @request = Request.find(params[:id])
   end
 
   def new
+    @request = Request.new
   end
 
   def create
+    @request = Request.new(request_params)
+    if @request.save
+      redirect_to request_path(@request)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:name, :email, :phone_number, :biography)
   end
 end
