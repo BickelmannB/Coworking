ec9bb05722ee4346677487d2eec8481a8d521172
bickelmann brice <bickelmann.b@gmail.com>
2019-07-12 16:02:17 +0200

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Coworking_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
-  config.action_mailer.default_url_options = { host: "www.gmail.com" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: "coworking-hexagonal.herokuapp.com" }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
