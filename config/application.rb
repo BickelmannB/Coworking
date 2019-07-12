fce1adcfa8bc69252d4696f843ea5761135f02e4
bickelmann brice <bickelmann.b@gmail.com>
2019-07-12 15:13:18 +0200

    mail setting

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,16 @@ module Coworking
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
+      config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :domain => "gmail.com",
+      user_name: ENV['GMAIL_ADDRESS'],
+      password: ENV['GMAIL_APP_PASSWORD'],
+      :authentication => "plain",
+      :enable_starttls_auto => true
+  }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
