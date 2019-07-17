1dd5d8eb618d4362185643c0d26b409e1ea666f5
bickelmann brice <bickelmann.b@gmail.com>
2019-07-17 18:09:18 +0200

    f*cking mailing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,7 +36,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'coworking-hexagonal.herokuapp.com', port: 587 }
+  config.action_mailer.default_url_options = { host: 'gmail.com', port: 587 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
