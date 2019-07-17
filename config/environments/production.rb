6b7ed121629cc059578e6349ae539f2f97bda1f6
bickelmann brice <bickelmann.b@gmail.com>
2019-07-17 17:40:48 +0200

    mailing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,10 +62,9 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "Coworking_#{Rails.env}"
-
-  config.action_mailer.perform_caching = false
-  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method     = :smtp
   config.action_mailer.default_url_options = { host: "coworking-hexagonal.herokuapp.com" }
+  # or your custom domain name eg. "www.yourdomain.com"
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
