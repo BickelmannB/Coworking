61985ee980062cbac0deb44346289c798495adb7
bickelmann brice <bickelmann.b@gmail.com>
2019-09-25 16:50:37 +0200

    pdfkit header unfinished

--- config/initializers/pdfkit.rb
+++ config/initializers/pdfkit.rb
@@ -10,18 +10,18 @@ PDFKit.configure do |config|
     margin_left: '10',
     margin_right: '10',
     header_spacing: '10',
-    header_html: render_header_footer("header"),
+    # header_html: render_header_footer("header"),
     footer_spacing: '2',
-    footer_html: render_header_footer("footer"),
+    # footer_html: render_header_footer("footer"),
     quiet: true
   }
 end
 end
 
-def render_header_footer(type)
-  compiled = ERB.new(File.read("#{Rails.root}/app/views/reservations/#{type}.html.erb")).result(binding)
-  file = Tempfile.new(["#{type}",".html"])
-  file.write(compiled)
-  file.rewind
-  file.path
-end
+# def render_header_footer(type)
+#   compiled = ERB.new(File.read("#{Rails.root}/app/views/reservations/#{type}.html.erb")).result(binding)
+#   file = Tempfile.new(["#{type}",".html"])
+#   file.write(compiled)
+#   file.rewind
+#   file.path
+# end
